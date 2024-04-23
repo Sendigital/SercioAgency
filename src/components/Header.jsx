bb6c8765e24360b2aba313b63354f867f5173003
Sendigital <sendigital.eth@gmail.com>
2024-04-23 13:01:20 +0100

    Updated Responsiveness Code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,7 @@
 import Nav from './Nav';
 import "./styles.css"
 import Banner from './Banner';
-import headerMan from '../assets/human.png';
-import top from "../assets/topImg.png";
-import center from "../assets/centerImg.png";
-import bottom from "../assets/bottomImg.png";
-import Amanda from "../assets/headshot1.jpeg";
-import William from "../assets/headshot2.jpeg";
-import Saepul from "../assets/headshot3.jpeg";
+
 
 
 const Header  = () => {
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,13 +6,20 @@ const Nav = () => {
         <nav>
             <h1>Sercio</h1>
 
+            <label for="toggle" className= "hamburger">&#9776;</label>
+            <input type="checkbox" id="toggle" />
+
+            <div className="navRight">
             <div className="navItems">
                 <a href="#">What we do</a>
                 <a href="#">Our Impact</a>
                 <a href="#">Our Thoughts</a>
             </div>
 
-            <button>Contact Us</button>
+            <div className="btn">
+                <button>Contact Us</button>
+            </div>
+            </div>
         </nav>
     )
 }
